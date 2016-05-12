69b43214602eff357ea2234468aec98173abb4f8
Todd Aspeotis <t@speot.is>
2016-05-12 19:12:40 +1000

    Skeleton structure

---
+++ BufferedDataReaderDotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("BufferedDataReaderDotNet")]
+[assembly: AssemblyProduct("BufferedDataReaderDotNet")]
+[assembly: AssemblyCopyright("Copyright © Todd Aspeotis 2016")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("22d83db4-d4ff-44d0-8c0f-1ac97bada3a8")]
